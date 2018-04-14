ee86f864fc6cdc0edab4629eaf07337fe77b8ad7
Vlad Frolenko <jonny_walker@mail.ru>
2018-04-14 12:47:05 -0700

    some changes with sql query

--- src/Controller.java
+++ src/Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 
@@ -16,47 +17,15 @@ public class Controller implements Initializable {
     static final String DB_URL = "jdbc:mysql://localhost/Alpinism";
     static final String USER = "root";
     static final String PASS = "";
+    ArrayList<Object> resultOfTable;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Controller controller = new Controller();
         btnExecute.setOnAction((e)->{
-            //showTable.setText("Hey bro");
+            String sql = sqlExecute.getText();
             try {
-                Connection conn = null;
-                Statement stmt = null;
-                //STEP 2: Register JDBC driver
-                Class.forName("com.mysql.jdbc.Driver");
-
-                //STEP 3: Open a connection
-                System.out.println("Connecting to database...");
-                conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-                //STEP 4: Execute a query
-                System.out.println("Creating statement...");
-                stmt = conn.createStatement();
-                String sql;
-                sql = "SELECT * FROM People";
-                ResultSet rs = stmt.executeQuery(sql);
-
-                //STEP 5: Extract data from result set
-                while (rs.next()) {
-                    //Retrieve by column name
-                    int id = rs.getInt("id");
-                    String first = rs.getString("FirstName");
-                    String last = rs.getString("LastName");
-                    String address = rs.getString("Address");
-
-                    //Display values
-                    System.out.println(id+" ");
-                    showTable.appendText(String.valueOf(id)+" ");
-                    showTable.appendText(first+" ");
-                    showTable.appendText(last+" ");
-                    showTable.appendText(address+"\n");
-                }
-                //STEP 6: Clean-up environment
-                rs.close();
-                stmt.close();
-                conn.close();
+                databaseQuery(sql);
+                showTable.setText(String.valueOf(resultOfTable));
             } catch (ClassNotFoundException e1) {
                 e1.printStackTrace();
             } catch (SQLException e1) {
@@ -64,7 +33,7 @@ public class Controller implements Initializable {
             }
         });
     }
-    private void databaseQuery() throws ClassNotFoundException, SQLException {
+    private void databaseQuery(String sql) throws ClassNotFoundException, SQLException {
         Connection conn = null;
         Statement stmt = null;
         //STEP 2: Register JDBC driver
@@ -77,25 +46,22 @@ public class Controller implements Initializable {
         //STEP 4: Execute a query
         System.out.println("Creating statement...");
         stmt = conn.createStatement();
-        String sql;
-        sql = "SELECT * FROM People";
+        //String sql;
+        //sql = "SELECT * FROM People";
         ResultSet rs = stmt.executeQuery(sql);
 
         //STEP 5: Extract data from result set
+        resultOfTable = new ArrayList<>();
         while (rs.next()) {
             //Retrieve by column name
             int id = rs.getInt("id");
             String first = rs.getString("FirstName");
             String last = rs.getString("LastName");
             String address = rs.getString("Address");
-
-            //Display values
-            System.out.println(id);
-            showTable.appendText(String.valueOf(id));
-            showTable.appendText(first);
-            showTable.appendText(last);
-            showTable.appendText(address);
-            showTable.appendText("/n");
+            resultOfTable.add(id);
+            resultOfTable.add(first);
+            resultOfTable.add(last);
+            resultOfTable.add(address);
         }
         //STEP 6: Clean-up environment
         rs.close();
